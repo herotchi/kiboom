ba9c4c462d6b1b3c4979f86e8cc5c3941472a54f
herotchi <hidenochion+github@gmail.com>
2023-04-21 19:01:44 +0900

    マージを解決するためのコミット

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -7,10 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use App\Models\Post;
+
 class TopController extends Controller
 {
     public function top()
     {
-        return view('top');
+        $postModel = new Post();
+        $posts = $postModel->getTopList();
+        //var_dump($posts);
+        return view('top', compact('posts'));
     }
 }
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,6 +2,41 @@
 
 @section('content')
 
+<<<<<<< Updated upstream
+=======
+<div class="row justify-content-center g-3">
+    <div class="col">
+        <div class="card">
+            <div class="card-header">最近のセッション</div>
+            <div class="card-body">
+                <div class="list-group">
+                @foreach($posts as $post)
+                {{ $post->user_id }}
+                @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="position-relative">
+    {{--<button type="button" id="postsAddBtn" class="btn btn-primary rounded-circle p-0 position-absolute bottom-0 end-0" 
+        data-bs-toggle="modal" data-bs-target="#postsAdd"
+        style="width:3rem;height:3rem;">
+        ＋
+    </button>--}}
+    <a class="btn btn-primary rounded-circle p-0 position-absolute bottom-0 end-0" 
+        style="width:3rem;height:3rem;" 
+        href="{{ route('posts.add') }}" role="button">
+        ＋
+    </a>
+</div>
+
+
+
+
+>>>>>>> Stashed changes
 <!-- ユーザー名変更モーダルの設定 -->
 @include('users.edit')
 
